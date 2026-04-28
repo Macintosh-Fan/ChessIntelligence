08f1aace6d1a39939e48da65f7d3837796e00ec1
Macintosh-Fan <toffeefur@icloud.com>
2026-04-27 23:45:17 -0700

    Added a checkerboard pattern to the chess board

--- test.js
+++ test.js
@@ -13,6 +13,10 @@ for (let i = 0; i < 8; i++) {
             chessBoardString[counter] === " "
                 ? "\u00A0"
                 : chessBoardString[counter];
+        chessBoardTableData.style.backgroundColor =
+            (Math.floor(counter / 8) + (counter % 8)) % 2 === 0
+                ? "#f0d9b5"
+                : "#946f51";
         chessBoardTableRow.append(chessBoardTableData);
 
         counter++;
