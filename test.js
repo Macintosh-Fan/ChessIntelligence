26755b72014a689078a0efa123a7bfe08d717b88
Macintosh-Fan <toffeefur@icloud.com>
2026-04-27 23:44:35 -0700

    Redesigned the chess board table

--- test.js
+++ test.js
@@ -2,15 +2,20 @@ let chessBoardTable = document.getElementById("chessBoard");
 let chessBoardString =
     "♜♞♝♛♚♝♞♜♟♟♟♟♟♟♟♟                                ♙♙♙♙♙♙♙♙♖♘♗♕♔♗♘♖";
 
+let chessBoardTableBody = document.createElement("tbody");
+chessBoardTable.append(chessBoardTableBody);
 let counter = 0;
 for (let i = 0; i < 8; i++) {
     let chessBoardTableRow = document.createElement("tr");
     for (let j = 0; j < 8; j++) {
         let chessBoardTableData = document.createElement("td");
-        chessBoardTableData.textContent = chessBoardString[counter];
+        chessBoardTableData.textContent =
+            chessBoardString[counter] === " "
+                ? "\u00A0"
+                : chessBoardString[counter];
         chessBoardTableRow.append(chessBoardTableData);
 
         counter++;
     }
-    chessBoardTable.append(chessBoardTableRow);
+    chessBoardTableBody.append(chessBoardTableRow);
 }
