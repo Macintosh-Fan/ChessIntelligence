e3b5bb577b53ae36fdb7fb092673e2e97e3ef11d
Macintosh-Fan <toffeefur@icloud.com>
2026-04-28 21:01:13 -0700

    Chess board overhaul again, switching from tables to divs

--- test.js
+++ test.js
@@ -1,25 +1,10 @@
-let chessBoardTable = document.getElementById("chessBoard");
+let chessBoard = document.getElementById("chessBoard");
 let chessBoardString =
     "♜♞♝♛♚♝♞♜♟♟♟♟♟♟♟♟                                ♙♙♙♙♙♙♙♙♖♘♗♕♔♗♘♖";
 
-let chessBoardTableBody = document.createElement("tbody");
-chessBoardTable.append(chessBoardTableBody);
-let counter = 0;
-for (let i = 0; i < 8; i++) {
-    let chessBoardTableRow = document.createElement("tr");
-    for (let j = 0; j < 8; j++) {
-        let chessBoardTableData = document.createElement("td");
-        chessBoardTableData.textContent =
-            chessBoardString[counter] === " "
-                ? "\u00A0"
-                : chessBoardString[counter];
-        chessBoardTableData.style.backgroundColor =
-            (Math.floor(counter / 8) + (counter % 8)) % 2 === 0
-                ? "#f0d9b5"
-                : "#946f51";
-        chessBoardTableRow.append(chessBoardTableData);
-
-        counter++;
-    }
-    chessBoardTableBody.append(chessBoardTableRow);
+for (let i = 0; i < 64; i++) {
+    chessBoard.children[i].textContent =
+        chessBoardString[i] === " " ? "\u00A0" : chessBoardString[i];
+    chessBoard.children[i].style.backgroundColor =
+        (Math.floor(i / 8) + (i % 8)) % 2 === 0 ? "#f0d9b5" : "#946f51";
 }
