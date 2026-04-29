d81568f627d19f65a3018a2d96e9d9359c7f0255
Macintosh-Fan <toffeefur@icloud.com>
2026-04-28 21:22:51 -0700

    Added variables to the test JavaScript

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
 let chessBoard = document.getElementById("chessBoard");
+let whiteSquareColor = "#f0d9b5";
+let blackSquareColor = "#946f51";
 let chessBoardString =
-    "♜♞♝♛♚♝♞♜♟♟♟♟♟♟♟♟                                ♙♙♙♙♙♙♙♙♖♘♗♕♔♗♘♖";
+    "♜♞♝♛♚♝♞♜" +
+    "♟♟♟♟♟♟♟♟" +
+    "        " +
+    "        " +
+    "        " +
+    "        " +
+    "♙♙♙♙♙♙♙♙" +
+    "♖♘♗♕♔♗♘♖";
 
 for (let i = 0; i < 64; i++) {
     chessBoard.children[i].textContent =
         chessBoardString[i] === " " ? "\u00A0" : chessBoardString[i];
     chessBoard.children[i].style.backgroundColor =
-        (Math.floor(i / 8) + (i % 8)) % 2 === 0 ? "#f0d9b5" : "#946f51";
+        (Math.floor(i / 8) + (i % 8)) % 2 === 0
+            ? whiteSquareColor
+            : blackSquareColor;
 }
